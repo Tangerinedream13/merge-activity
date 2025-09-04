33a1375fd14dce674e90b2ab99ac492b1827e4e2
Mling0502 <mlingle@unca.edu>
2025-09-04 10:32:25 -0400

    Stuff

--- my_code.py
+++ my_code.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def say_hello(name: str) -> None:
     print(f"Hello {name}!")
 
@@ -7,3 +8,12 @@ if __name__ == "__main__":
     say_hello("Ruth")
     say_hello("Larry")
     say_hello("Helen")
+=======
+def say_hello(times) -> None:
+     for i in range(times):
+          print("hello world!")
+
+
+if __name__ == "__main__":
+     say_hello(3)
+>>>>>>> 1ca4b12 (Maggie did stuff)
